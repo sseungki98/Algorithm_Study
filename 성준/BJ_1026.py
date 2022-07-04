5a0231dfa46503424e3a385b71f88bb176ea67bf
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-07-04 18:44:47 +0900

    index() 함수와 pop() 함수를 통한 문제풀이

--- 성준/BJ_1026.py
+++ 성준/BJ_1026.py
@@ -1,12 +1,8 @@
 N=int(input())
 A_list=list(map(int,input().split()))
 B_list=list(map(int,input().split()))
-
-A_list.sort() #오른차순으로 정렬
-B_list.sort(reverse=True) #내림차순으로 정렬
 answer=0
 
-for i in range(N):  #최솟값을 얻기 위해서는, 큰 값을 작은값들과 곱해졌을 때 최소의 결과를 얻을 수 있으므로
-    answer+=A_list[i]*B_list[i]   #가장 큰 값은 가장 작은 값과, 그 다음 큰 값은 그 다음 작은 값과 매칭시켜준다.
-    
+for i in range(N):
+    answer+=A_list.pop(A_list.index(min(A_list)))*B_list.pop(B_list.index(max(B_list))) #A의 최소값과 B의 최대값을 빼와주며 곱해준다.
 print(answer)
