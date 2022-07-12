b8876d95af6b0981c724f70bb6a63f40c5580df4
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-07-12 18:11:30 +0900

    예외처리를 꼼꼼히하여 가능한 경우의 수들을 제외하며 최소값을 찾는 문제풀이

---
+++ 성준/BJ_1590.py
@@ -0,0 +1,25 @@
+N,T=map(int,input().split())
+answer=1000000
+
+for _ in range(N):
+    S,I,C=map(int,input().split())  #시작시간, 간격, 대수
+    if S >= T:  #버스가 아직 운행을 시작하지 않은 경우
+        if answer == -1:
+            answer=S-T
+        else:
+            answer=min(answer,S-T) 
+    else:   #버스가 이전부터 운행을 시작한 경우
+        if S+((C-1)*I) < T: #마지막 버스의 출발시간이 영식의 도착시간보다 빠른 경우
+            if answer == 1000000:
+                answer=-1
+        else:
+            while C:
+                S+=I    #다음 버스 도착시간
+                C-=1    #버스 대수 감소
+                if S >= T:  #버스가 도착시간 이후 도착한 경우
+                    if answer == -1:    #이전에 -1을 경우 예외처리
+                        answer=S-T
+                    else:
+                        answer=min(answer,S-T)
+                        break
+print(answer)
