c4a9193f2536cc73c7a89fafff44f0a5effb9d66
Hongjun <89785414+junBcomputerstory@users.noreply.github.com>
2022-08-13 16:25:07 +0900

    4와 7로만 이루어진숫자중에 주어진 번호번째로 작은 값을 구하라,구현

--- 홍준/BJ_2877.java
+++ 홍준/BJ_2877.java
@@ -1,1 +1,25 @@
+import java.io.*;
+import java.util.*;
 
+public class BJ_2877 {
+
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+        n=n+1;
+        StringBuilder sb = new StringBuilder();
+        int num = 0;
+        while(n!=0){
+            num = n%2;
+            sb.append(num);
+            n = n/2;
+        }
+        StringBuilder result = new StringBuilder();
+        for(int i=sb.toString().length()-2;i>=0;i--){
+            if(sb.charAt(i) == '0') result.append(4);
+            else result.append(7);
+        }
+        System.out.println(result.toString());
+	}
+}
