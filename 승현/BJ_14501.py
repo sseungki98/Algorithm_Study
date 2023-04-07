7886782cf709bbc098cf5f7ac6dff630304e0c03
sseungki98 <appp_le@naver.com>
2023-04-07 18:38:45 +0900

    DP를 이용한 문제 분할 풀이

--- 승현/BJ_14501.py
+++ 승현/BJ_14501.py
@@ -5,15 +5,10 @@ for _ in range(N):
     temp = list(map(int, input().split()))
     schedule.append([temp[0], temp[1]])
 
-money = []
+dp = [0 for _ in range(N+1)]
 for i in range(N):
-    temp_money = 0
-    j = i
-    while True:
-        if j > N-1:
-            break
-        temp_money = schedule[j][1]
-        j += schedule[j][0]
-    money.append(temp_money)
-print(schedule)
-print(money)
+    for j in range(i + schedule[i][0], N+1):
+        if dp[j] < dp[i] + schedule[i][1]:
+            dp[j] = dp[i] + schedule[i][1]
+
+print(dp[-1])
