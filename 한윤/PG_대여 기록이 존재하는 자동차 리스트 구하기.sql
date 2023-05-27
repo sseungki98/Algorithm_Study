9d1812ecc8edcf67c96f0648c4335ab9507db205
최한윤 <chlgksdbs98@naver.com>
2023-05-27 09:24:26 +0900

    join 연산과 날짜 범위 비교를 통한 쿼리문 작성

--- 한윤/PG_대여 기록이 존재하는 자동차 리스트 구하기.sql
+++ 한윤/PG_대여 기록이 존재하는 자동차 리스트 구하기.sql
@@ -4,3 +4,4 @@ from car_rental_company_car c join car_rental_company_rental_history h
 using (car_id)
 where c.car_type='세단' and (h.start_date between '2022-10-01' and '2022-10-31')
 order by c.car_id desc
+
