45d01bd33194e63f29c7bb8e5c4ff6b150ee2921
Lee SeungHyun <79951703+sseungki98@users.noreply.github.com>
2023-10-13 01:51:32 +0900

    [Gold V] Title: 배, Time: 100 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.py
+++ 백준/Gold/1092. 배/배.py
@@ -1,30 +1,26 @@
-n = int(input())
-crane = list(map(int, input().split()))
-m = int(input())
-box = list(map(int, input().split()))
+N = int(input())
+crains = sorted(list(map(int, input().split())), reverse=True)
+M = int(input())
+boxes = sorted(list(map(int, input().split())), reverse=True)
 
-# ⚡ 내림차순 정렬
-crane.sort(reverse = True)
-box.sort(reverse = True)
-
-time = 0 # 시간
-checked = [0 for _ in range(m)] # 박스를 옮겼는지 여부
-count = 0 # 옮긴 박스의 개수 
-
-positions = [0] * n
-
-if max(box) > max(crane):
+if boxes[0] > crains[0]:
     print(-1)
-else:
-    while count < len(box):
-        for i in range(n): # 크레인에 대하여
-            while positions[i] < len(box):
-            # 아직 안 옮긴 박스 중에서, 옮길 수 있는 박스를 만날 때까지 반복
-                if not checked[positions[i]] and crane[i] >= box[positions[i]]:
-                    checked[positions[i]] = True
-                    positions[i] += 1
-                    count += 1
-                    break
-                positions[i] += 1
-        time += 1
-    print(time)   
+    exit()
+
+move_count = 0
+while boxes:
+    for item in crains:
+        if boxes and boxes[-1] > item:
+            continue
+        for box in boxes:
+            if item >= box:
+                boxes.remove(box)
+                break
+
+    move_count += 1
+
+print(move_count)
+
+
+
+
